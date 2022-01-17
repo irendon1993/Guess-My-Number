d248fb62582b28c3ab1ba746a015a336ecef02fc
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-17 11:42:43 -0500

    Manipulating css styles with js

--- script.js
+++ script.js
@@ -20,10 +20,17 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
